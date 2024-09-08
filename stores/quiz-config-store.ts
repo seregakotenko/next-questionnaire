2883b37057467316567f5200694079adf166f902
serhii.kotenko <serega.kotenko@gmail.com>
2024-09-09 01:50:53 +0300

    feat: persist store in Local Storage

--- stores/quiz-config-store.ts
+++ stores/quiz-config-store.ts
@@ -2,6 +2,7 @@ import { createStore } from 'zustand/vanilla';
 import { Screen } from '@/types/api/config';
 import { quizConfig } from '@/mock/quizConfig';
 import { AnswerType, ConfigState, ConfigStore } from '@/types/store';
+import { persist } from 'zustand/middleware';
 
 export const initConfigStore = (): ConfigState => {
   return {
@@ -16,11 +17,29 @@ export const defaultInitState: ConfigState = {
 };
 
 export const createConfigStore = (initState: ConfigState = defaultInitState) => {
-  return createStore<ConfigStore>()((set) => ({
-    ...initState,
-    setAnswer: (questionId: string, answer: string) =>
-      set((state) => ({ answers: updateAnswers(state.answers, questionId, answer) })),
-  }));
+  return createStore<ConfigStore>()(
+    persist(
+      (set) => ({
+        ...initState,
+        setAnswer: (questionId: string, answer: string) =>
+          set((state) => ({ answers: updateAnswers(state.answers, questionId, answer) })),
+      }),
+      {
+        version:
+          Number(process.env.NEXT_PUBLIC_PERSIST_VERSION) || Math.floor(Math.random() * 1000) + 1,
+        migrate: (persistedState: any, version) => {
+          if (version !== Number(process.env.NEXT_PUBLIC_PERSIST_VERSION)) {
+            delete persistedState.config;
+            delete persistedState.answers;
+          }
+
+          return persistedState;
+        },
+        name: 'config-store', // name of the item in the storage (must be unique)
+        skipHydration: false, // do not rehydrate this store after a full page load, we will rehydrate it manually
+      },
+    ),
+  );
 };
 
 function updateAnswers(answers: AnswerType[], questionId: string, value: string) {
